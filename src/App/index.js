a94533b889d7a8ff3b171f71f7c41a925566626e
ndminhdev <ndminhdev@gmail.com>
2021-03-27 23:07:06 +0700

    Re-structure src folder

--- src/App/index.js
+++ src/App/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Field from '../common/Field';
+import Field from '@/components/Field';
 
 import './style.scss';
 
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import './index.scss';
-import App from './components/App';
+import App from './App';
 
 ReactDOM.render(
   <Router>
