1cb7a13cda05781704771543f98043e66d891704
ndminhdev <ndminhdev@gmail.com>
2021-03-29 00:44:29 +0700

    Create Overlay

---
+++ src/components/Overlay/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom';
+
+import './style.scss';
+
+const Overlay = ({ component: OverlayComponent, show, toggleShow }) =>
+  show
+    ? ReactDOM.createPortal(
+        <React.Fragment>
+          <div className="overlay__backdrop"></div>
+          <OverlayComponent toggleShow={toggleShow} />
+        </React.Fragment>,
+        document.getElementById('portal-overlay')
+      )
+    : null;
+
+Overlay.propTypes = {
+  component: PropTypes.func.isRequired,
+  show: PropTypes.bool.isRequired,
+  toggleShow: PropTypes.func.isRequired
+};
+
+export default Overlay;
