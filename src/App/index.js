408fab85d4b5adaeea9ebb13d344e1376c33a5a1
ndminhdev <ndminhdev@gmail.com>
2021-03-28 08:46:43 +0700

    Use custom icons as object

--- src/App/index.js
+++ src/App/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
-import GoogleIcon from '@/assets/icons/google.svg';
-import FacebookIcon from '@/assets/icons/facebook.svg';
+import { socialIcons } from '../utils/icon';
 import SocialButton from '@/components/SocialButton';
 
 import './style.scss';
@@ -9,8 +8,10 @@ import './style.scss';
 const App = () => {
   return (
     <div className="App">
-      <SocialButton icon={GoogleIcon}>Sign in with Google</SocialButton>
-      <SocialButton icon={FacebookIcon}>Sign in with Facebook</SocialButton>
+      <SocialButton icon={socialIcons.Google}>Sign in with Google</SocialButton>
+      <SocialButton icon={socialIcons.Facebook}>
+        Sign in with Facebook
+      </SocialButton>
     </div>
   );
 };
---
+++ src/utils/icon.js
@@ -0,0 +1,7 @@
+import Google from '@/assets/icons/google.svg';
+import Facebook from '@/assets/icons/facebook.svg';
+
+export const socialIcons = {
+  Google,
+  Facebook
+};
