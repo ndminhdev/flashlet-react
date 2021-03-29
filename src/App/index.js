f5208de3ec86743e3f5576406aea162b9125891c
ndminhdev <ndminhdev@gmail.com>
2021-03-29 09:52:41 +0700

    Test sign up api

--- src/App/index.js
+++ src/App/index.js
@@ -1,14 +1,26 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import SignInBox from '@/features/Auth/SignInBox';
 import { Overlay, Button } from '@/components';
 import { useOverlay } from '@/hooks';
+import { UserAPI } from '@/api';
 
 import './style.scss';
 
 const App = () => {
   const [show, toggleShow] = useOverlay();
   const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState({});
+
+  useEffect(async () => {
+    const data = await UserAPI.signUp({
+      email: 'test@gmail.com',
+      name: 'Dang Minh',
+      password: '@6991hnim'
+    });
+
+    setUser(data.user);
+  });
 
   return (
     <div className="App">
@@ -17,6 +29,7 @@ const App = () => {
       </Button>
       <button onClick={toggleShow}>Show</button>
       <Overlay component={SignInBox} show={show} toggleShow={toggleShow} />
+      <div>{JSON.stringify(user)}</div>
     </div>
   );
 };
