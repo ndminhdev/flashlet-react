0b128c94a2da52ba1cc28892508f52104486733e
ndminhdev <ndminhdev@gmail.com>
2021-04-26 19:26:45 +0700

    Fix styles

--- src/pages/AccountPage/index.js
+++ src/pages/AccountPage/index.js
@@ -34,22 +34,24 @@ const AccountPage = () => {
         <h1>Account Settings</h1>
         <div className="account__profile">
           <h3 className="account__heading">Profile</h3>
-          <div className="account__profile-fields">
+          <div className="account__profile-container">
             <ProfileImageForm
               image={user.profileImage || user.profileImageDefault}
             />
-            <ProfileTextForm
-              field="email"
-              label="Email address"
-              value={user.email}
-              validationSchema={emailSchema}
-            />
-            <ProfileTextForm
-              field="name"
-              label="Full name"
-              value={user.name}
-              validationSchema={nameSchema}
-            />
+            <div className="account__profile-fields">
+              <ProfileTextForm
+                field="email"
+                label="Email address"
+                value={user.email}
+                validationSchema={emailSchema}
+              />
+              <ProfileTextForm
+                field="name"
+                label="Full name"
+                value={user.name}
+                validationSchema={nameSchema}
+              />
+            </div>
           </div>
         </div>
         <div className="account__password">
