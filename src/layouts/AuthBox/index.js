54c5268ed7fc665aa7c8de8fa38cabed0eccdb0b
ndminhdev <ndminhdev@gmail.com>
2021-04-29 22:37:07 +0700

    Use useTransition instead of useSpring

--- src/layouts/AuthBox/index.js
+++ src/layouts/AuthBox/index.js
@@ -1,29 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useSpring, a } from '@react-spring/web';
+import { useTransition, a } from '@react-spring/web';
 
 import './style.scss';
+import { useOverlay } from '@/hooks';
 import icons from '@/utils/icons';
 
 const CloseIcon = icons.Close;
 
 const AuthBox = ({ children, title, handleClose }) => {
-  const props = useSpring({
-    from: { transform: 'translateY(-20px)', opacity: 0.8 },
-    to: { transform: 'translateY(0)', opacity: 1 },
-    config: { duration: 200 }
+  const { signInOverlayShown, signUpOverlayShown } = useOverlay();
+  const isShown = signInOverlayShown || signUpOverlayShown;
+
+  const transitions = useTransition(isShown, {
+    from: { transform: 'translateY(-20px)', opacity: 0.6 },
+    enter: { transform: 'translateY(0)', opacity: 1 },
+    leave: { transform: 'translateY(-20px)', opacity: 0.6 },
+    reverse: isShown,
+    config: { duration: 250 }
   });
 
-  return (
-    <a.div style={props} className="authbox">
-      <div className="authbox__header">
-        <h1 className="authbox__title">{title}</h1>
-        <button className="authbox__close-btn" onClick={handleClose}>
-          <CloseIcon className="authbox__close-icon" />
-        </button>
-      </div>
-      <div className="authbox__body">{children}</div>
-    </a.div>
+  return transitions(
+    (styles, item) =>
+      item && (
+        <a.div style={styles} className="authbox">
+          <div className="authbox__header">
+            <h1 className="authbox__title">{title}</h1>
+            <button className="authbox__close-btn" onClick={handleClose}>
+              <CloseIcon className="authbox__close-icon" />
+            </button>
+          </div>
+          <div className="authbox__body">{children}</div>
+        </a.div>
+      )
   );
 };
 
